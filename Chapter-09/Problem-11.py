b9449e0964b7dd78c01ef276fae0cf947835baab
Dharmendra Pandit <dharmendrapanditx@gmail.com>
2025-09-15 14:46:14 +0545

    Solving problem no 11

--- Chapter-09/Problem-11.py
+++ Chapter-09/Problem-11.py
@@ -1,1 +1,7 @@
 # 11. Write a python program to rename a file to “renamed_by_ python.txt.
+import os
+def rename_file( old_name, new_name):
+    os.rename(old_name, new_name)
+    print("File renamed sucessfully")
+
+rename_file("renamed.txt", "renamed_by_python.txt")
