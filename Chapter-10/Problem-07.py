5efb6231b2afd29ffb36417def23c2e9978ad1f5
Dharmendra Pandit <dharmendrapanditx@gmail.com>
2025-10-22 13:22:15 +0545

    probelm 6 solved

---
+++ Chapter-10/Problem-07.py
@@ -0,0 +1,17 @@
+class Method:
+    def __init__(slf, company):
+        slf.company = company
+
+    def Harry(harry):
+        print(f"Harry work on {harry.company}")
+     
+    def Information(harr, Name , Address):
+        print(f" whos CEo name is {Name} and Adress is {Address}")
+    
+        
+
+a = Method("Neura")
+a.Harry()
+a.Information("Dharmendra", "Rajawatiya")
+
+        
